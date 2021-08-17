b5bb92e4442c48e3f38c05aaac7b15aa7366ffae
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-17 16:38:16 -0700

    finished lab 01

--- src/Main.js
+++ src/Main.js
@@ -1,42 +1,40 @@
+import React from "react";
+import HornedBeast from "./HornedBeasts";
 
-
-class Main extends Component {
+class Main extends React.Component {
   render()  {
     return (
-    <>  
-      <h1>Message here: {this.props.message}</h1>;
-      <BeastImage image_url={beastImages[1].image_url} /> 
-      <BeastImage />
-      <BeastImage />
-      <BeastImage />
-    </>
-  })
+      <>  
+        <HornedBeast />
+        <HornedBeast />
+      </>
+    );
+  }
 }
+// class BeastImage extends Component {
 
-class BeastImage extends Component {
+//     constructor(props){
+//         super(constructor);
+//         this.state= {
+//             'status': "yay"
+//         }
 
-    constructor(props){
-        super(constructor);
-        this.state= {
-            'status': "yay"
-        }
+//     handleClick =()=> {
+//         const newStatus = this.state.status === 'Nay' ? 'Yay' :  'Nay';
+//         this.setState({
+//             status: newStatus
+//         })
+//     }
 
-    handleClick =()=> {
-        const newStatus = this.state.status === 'Nay' ? 'Yay' :  'Nay';
-        this.setState({
-            status: newStatus
-        })
-    }
-
-    }
-    render() {
-        return 
-        <div onClick=(this.handleClick)>
-        <img src= {this.props.image_url />
-        <p>{this.state.status}</p>
-        </div>
-    }
-}
+//     }
+//     render() {
+//         return 
+//         <div onClick=(this.handleClick)>
+//         <img src= {this.props.image_url />
+//         <p>{this.state.status}</p>
+//         </div>
+//     }
+// }
 
 
 
