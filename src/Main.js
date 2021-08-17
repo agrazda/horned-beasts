8ea7fe96f9a8a4c0645a8e33d298dd4ea7d555a6
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-17 13:26:21 -0700

    Follow Along day 2

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,42 @@
 
-function Main(props){
-    return(
-        <> 
-      <h1>(props.title)</h1> 
-      <h2>(props.imageUrl)</h2>
-      <h2>(props.description)</h2>
-      <p></p>
-      
-       </>
-    )
+
+class Main extends Component {
+  render()  {
+    return (
+    <>  
+      <h1>Message here: {this.props.message}</h1>;
+      <BeastImage image_url={beastImages[1].image_url} /> 
+      <BeastImage />
+      <BeastImage />
+      <BeastImage />
+    </>
+  })
 }
 
+class BeastImage extends Component {
 
+    constructor(props){
+        super(constructor);
+        this.state= {
+            'status': "yay"
+        }
+
+    handleClick =()=> {
+        const newStatus = this.state.status === 'Nay' ? 'Yay' :  'Nay';
+        this.setState({
+            status: newStatus
+        })
+    }
+
+    }
+    render() {
+        return 
+        <div onClick=(this.handleClick)>
+        <img src= {this.props.image_url />
+        <p>{this.state.status}</p>
+        </div>
+    }
+}
 
 
 
