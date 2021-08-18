d6789848241c10338c1984aab2f4729bf66e12f9
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-17 17:52:17 -0700

    lab 02

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import React from 'react';
-// import beastImages from './data.json';
+import beastImages from './data.json';
 
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      beastImages: beastImages, 
+    };
+  }
   render(){
     return (
       <>
         <Header title='Horned Beasts' />
-        <Main message= 'Add Something'/>
+        <Main beastImages= {this.state.beastImages}/>
         <Footer />
       </>
     );
